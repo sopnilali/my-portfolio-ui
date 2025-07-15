81a5e604db965a48140f49a601d45a39930a751e
sopnilali <sopnilali24@gmail.com>
2025-07-15 18:39:19 +0600

    sdsd

--- src/app/(HomePageLayout)/experience/page.tsx
+++ src/app/(HomePageLayout)/experience/page.tsx
@@ -4,7 +4,21 @@ import React from 'react'
 
 export const metadata : Metadata = {
   title: "My Experience",
-  description: "My Experience"
+  description: "My Experience",
+  keywords: ['Md. Abdul Adud', 'Web Developer', 'Software Engineer', 'React Developer', 'Next.js Developer', 'Node.js Developer', 'MongoDB Developer', 'Express.js Developer', 'Tailwind CSS Developer', 'HTML Developer', 'CSS Developer', 'JavaScript Developer', 'TypeScript Developer', 'React Native Developer', 'Flutter Developer', 'Python Developer', 'Java Developer', 'C Developer', 'C++ Developer', 'C# Developer', 'PHP Developer', 'SQL Developer', 'NoSQL Developer', 'Docker Developer', 'Kubernetes Developer', 'AWS Developer', 'Azure Developer', 'GCP Developer', 'DevOps Developer', 'Cybersecurity Developer', 'Blockchain Developer', 'Artificial Intelligence Developer', 'Machine Learning Developer', 'Data Science Developer', 'Data Analyst Developer', 'Data Engineer Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer' ],
+  openGraph: {
+    title: "My Experience",
+    description: "My Experience",
+    url: 'https://www.linkedin.com/in/ami-abdul-adud',
+    images: ['https://res.cloudinary.com/dbebxi5vg/image/upload/v1752128122/documents/obkaer3eaq1u8fscapcl.jpg'],
+  },
+  alternates: {
+    canonical: 'https://www.linkedin.com/in/ami-abdul-adud',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 const ExperiencePage = () => {
--- src/app/(HomePageLayout)/page.tsx
+++ src/app/(HomePageLayout)/page.tsx
@@ -2,12 +2,43 @@
 import ExperienceList from '@/components/Modules/ExperienceAndEducationList'
 import HomePageIndex from '@/components/Modules/HomePage'
 import HeroSection from '@/components/Modules/HomePage/HeroSection'
+import { Metadata } from 'next'
 
 import React from 'react'
 
-export const metadata = {
+export const metadata : Metadata = {
   title: 'Welcome to my portfolio | Md. Abdul Adud ',
   description: 'I am a web developer with a passion for building web applications.',
+  keywords: ['Md. Abdul Adud', 'Web Developer', 'Software Engineer', 'React Developer', 'Next.js Developer', 'Node.js Developer', 'MongoDB Developer', 'Express.js Developer', 'Tailwind CSS Developer', 'HTML Developer', 'CSS Developer', 'JavaScript Developer', 'TypeScript Developer', 'React Native Developer', 'Flutter Developer', 'Python Developer', 'Java Developer', 'C Developer', 'C++ Developer', 'C# Developer', 'PHP Developer', 'SQL Developer', 'NoSQL Developer', 'Docker Developer', 'Kubernetes Developer', 'AWS Developer', 'Azure Developer', 'GCP Developer', 'DevOps Developer', 'Cybersecurity Developer', 'Blockchain Developer', 'Artificial Intelligence Developer', 'Machine Learning Developer', 'Data Science Developer', 'Data Analyst Developer', 'Data Engineer Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer', 'Data Governance Developer', 'Data Privacy Developer', 'Data Management Developer', 'Data Analysis Developer', 'Data Science Developer', 'Data Engineering Developer', 'Data Visualization Developer', 'Data Mining Developer', 'Data Warehousing Developer', 'Data Modeling Developer', 'Data Integration Developer', 'Data Quality Developer', 'Data Security Developer' ],
+  openGraph: {
+    title: 'Welcome to my portfolio | Md. Abdul Adud ',
+    description: 'I am a web developer with a passion for building web applications.',
+    url: 'https://www.linkedin.com/in/ami-abdul-adud',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dbebxi5vg/image/upload/v1752128122/documents/obkaer3eaq1u8fscapcl.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Md. Abdul Adud'
+      }
+    ],
+    siteName: 'Md. Abdul Adud',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Welcome to my portfolio | Md. Abdul Adud ',
+    description: 'I am a web developer with a passion for building web applications.',
+    images: ['https://res.cloudinary.com/dbebxi5vg/image/upload/v1752128122/documents/obkaer3eaq1u8fscapcl.jpg'],
+  },
+  alternates: {
+    canonical: 'https://mdabduladudui.vercel.app/',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 const HomePage = () => {
